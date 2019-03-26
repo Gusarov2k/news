5f5f42a3a2393dc17e034f5f442f5e6e368fd4a0
gusarov2k <motoface@yandex.ru>
2019-03-26 14:13:56 +0300

    add options for rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+require 'capybara/rspec'
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.include Sorcery::TestHelpers::Rails
+  config.include Sorcery::TestHelpers::Rails::Controller, type: :controller
+  config.include Sorcery::TestHelpers::Rails::Integration, type: :feature
+  config.include Sorcery::TestHelpers::Rails::Integration, type: :model
+
+  config.before(:all) do
+    DatabaseCleaner.clean
+  end
+end
