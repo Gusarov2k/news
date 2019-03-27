0bcab27eccc5a4ade736143c7b2c14ddfb4140a3
gusarov2k <motoface@yandex.ru>
2019-03-27 15:33:24 +0300

    add validates

--- spec/factories/top_news.rb
+++ spec/factories/top_news.rb
@@ -1,8 +1,11 @@
 FactoryBot.define do
   factory :top_news do
-    title { "MyString" }
-    announcement { "MyString" }
-    context { "MyText" }
-    created { "2019-03-26" }
+    title { 'MyString is' }
+    announcement { 'MyString good or bad' }
+    context do
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et'
+    end
+    created { '2019-03-26' }
   end
 end
