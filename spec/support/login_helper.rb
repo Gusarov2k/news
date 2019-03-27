46f16f11bda828498faff9c37926038f15c4b53d
gusarov2k <motoface@yandex.ru>
2019-03-27 12:45:00 +0300

    add tests

--- spec/support/login_helper.rb
+++ spec/support/login_helper.rb
@@ -1,6 +1,6 @@
 module LogInHelper
   def log_in(email, password, action)
-    visit log_in_path(locale: :en)
+    visit log_in_path
     fill_in 'Email', with: email
     fill_in 'Password', with: password
     click_button action
